6c6c44ee835dcb3dfee41f406674bd5f2ae74329
Mariana <marianagomesluz@gmail.com>
2022-04-04 18:46:34 -0300

    AAAAA

--- sources/ppos.c
+++ sources/ppos.c
@@ -2,21 +2,21 @@
 #include "ppos_core.h"
 
 task_t *filaTarefas;
-task_t main;
+task_t mainTask;
 // Inicializar as variáveis e o buffer do printf
 void ppos_init()
 {
     //Inicialização das variáveis
-   filaTarefas = NULL;
-    
-    main->id = maiorId + 1;                                       // define a id da task
-    main->status = 0;                                             // Define o status da task a ser criada como Pronta
-    getcontext(&(main->context));                                 // Copia o contexto atual e copia para contextoTask
-    main->context.uc_stack.ss_flags = 0;                          // Deixa como zero a máscara das flags
-    main->context.uc_link = 0;                                    // Quando a task terminar ela não tem para onde voltar, então deixa como 0
-    main->preemptable = 0; // Define a variavel preempable da task do elemento de fila como 0, ou seja não preemptável
+    filaTarefas = NULL;
 
-    queue_append((&filaTarefas), main);
+    mainTask.id = 0;                        // define a id da task
+    mainTask.status = 0;                    // Define o status da task a ser criada como Pronta
+    getcontext(&(mainTask.context));        // Copia o contexto atual e copia para contextoTask
+    mainTask.context.uc_stack.ss_flags = 0; // Deixa como zero a máscara das flags
+    mainTask.context.uc_link = 0;           // Quando a task terminar ela não tem para onde voltar, então deixa como 0
+    mainTask.preemptable = 0;               // Define a variavel preempable da task do elemento de fila como 0, ou seja não preemptável
+
+    queue_append((queue_t **)(&filaTarefas), ((queue_t *)(&mainTask)));
     //Inicilização do buffer do printf
     setvbuf(stdout, 0, _IONBF, 0);
 }
@@ -30,28 +30,29 @@ int task_create(task_t *task, void (*start_routine)(void *), void *arg)
     task_t *primeiraTask = filaTarefas;
     if (filaTarefas != NULL)
     {
-    	task_t *atual = filaTarefas->next; // atual, recebe a task do elemento de fila atual
-    	maiorId = filaTarefas->id;     // maiorId vai servir para definir o proximo id a ser usado,
+        task_t *atual = filaTarefas->next; // atual, recebe a task do elemento de fila atual
+        maiorId = filaTarefas->id;         // maiorId vai servir para definir o proximo id a ser usado,
 
-    	while (atual->next != primeiraTask) // Acha a maior task->id de todos os elementos de fila
-    	{
-      	 	if (atual->id > maiorId)
-       		{
-            		maiorId = atual->id;
-        	}
-        	atual = atual->next;
-    	}
+        while (atual->next != primeiraTask) // Acha a maior task->id de todos os elementos de fila
+        {
+            if (atual->id > maiorId)
+            {
+                maiorId = atual->id;
+            }
+            atual = atual->next;
+        }
     }
-    task->id = maiorId + 1;                                       // define a id da task
-    task->status = 0;                                             // Define o status da task a ser criada como Pronta
-    getcontext(&(task->context));                                 // Copia o contexto atual e copia para contextoTask
-    task->context.uc_stack.ss_flags = 0;                          // Deixa como zero a máscara das flags
-    task->context.uc_link = 0;                                    // Quando a task terminar ela não tem para onde voltar, então deixa como 0
-    makecontext(&(task->context), start_routine, 1, (char *)arg); // Cria o contexto da task no endereço do contextoTask
+    task->id = maiorId + 1;                                                       // define a id da task
+    task->status = 0;                                                             // Define o status da task a ser criada como Pronta
+    getcontext(&(task->context));                                                 // Copia o contexto atual e copia para contextoTask
+    task->context.uc_stack.ss_flags = 0;                                          // Deixa como zero a máscara das flags
+    task->context.uc_link = 0;                                                    // Quando a task terminar ela não tem para onde voltar, então deixa como 0
+    makecontext(&(task->context), (void (*)(void))start_routine, 1, (char *)arg); // Cria o contexto da task no endereço do contextoTask
     // Define o contexto da task no elemento de fila como o contextoTask
     task->preemptable = 0; // Define a variavel preempable da task do elemento de fila como 0, ou seja não preemptável
 
-    queue_append((&filaTarefas), task);
+    queue_append((queue_t **)(&filaTarefas), ((queue_t *)(&task)));
+    return 0;
 }
 
 //Muda para outra tarefa, transfere o processador para a tarefa indicada.
@@ -94,28 +95,25 @@ void task_exit(int exit_code)
     task_t *atual = filaTarefas, *proxima = filaTarefas;
     if (filaTarefas == NULL)
     {
-        fprintf(stderr, "Fila vazia?\n");
-        return -3;
+        fprintf(stderr, "Fila vazia? - task_exit\n");
     }
     for (;;)
     {
         proxima = proxima->next;
         if (proxima == NULL)
         {
-            fprintf(stderr, "Elemento nulo detectado - task_switch\n");
-            return -1;
+            fprintf(stderr, "Elemento nulo detectado - task_exit\n");
         }
         if (proxima == filaTarefas)
         {
-            fprintf(stderr, "Main nao encontrada - task_switch\n");
-            return -2;
+            fprintf(stderr, "Main nao encontrada - task_exit\n");
         }
         /* Se encontrou a main, break */
         if (proxima->id == 0)
             break;
     }
     /* Removemos a tarefa e desalocamos */
-    queue_remove(&filaTarefas, atual);
+    queue_remove((queue_t **)&filaTarefas, ((queue_t *)(&atual)));
     /* Trocamos para a main */
     if (task_switch(proxima) < 0)
         fprintf(stderr, "Erro ao trocar para a main - task_exit\n");
