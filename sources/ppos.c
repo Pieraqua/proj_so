728b330bcca667e04fc4658421f73317a8493d36
Paulo Sergio Avila Junior <pieraqua@pieraqua.pieraqua.com>
2022-04-04 16:33:34 -0300

    Movido tasks.c para sources/, esqueleto de funcoes criados

---
+++ sources/ppos.c
@@ -0,0 +1,26 @@
+#include "ppos.h"
+
+void ppos_init()
+{
+	
+}
+
+int task_create(task_t *task, void (*start_routine)(void *), void *arg)
+{
+
+}
+
+int task_switch(task_t *task)
+{
+
+}
+
+void task_exit(task_t *task)
+{
+
+}
+
+int task_id()
+{
+
+}
