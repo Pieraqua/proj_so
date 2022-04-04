65dcae4ca1fb254ef2e04bf7c41570f61a4a2c62
Mariana <marianagomesluz@gmail.com>
2022-04-04 18:17:24 -0300

    Revisoes

--- sources/ppos.c
+++ sources/ppos.c
@@ -67,54 +67,70 @@ int task_create(task_t *task, void (*start_routine)(void *), void *arg)
 int task_switch(task_t *task)
 {
 
-	filatask_t *proxima = filaTarefas, *atual = filaTarefas;		
-
-    	for(;;)
-    	{
-		proxima = proxima->next;
-		if(proxima == NULL){
-	       		fprintf(stderr, "Elemento nulo detectado - task_switch\n");
-			return -1;
-		}
-		if(proxima == filaTarefas){
-			fprintf(stderr, "Tarefa nao encontrada - task_switch\n");
-			return -2;
-		}
-		if(proxima == task) break;
-    	}
-	/* Setamos a tarefa a ser trocada como a tarefa ativa */
-	filaTarefas = proxima;
-	/* E trocamos de contexto, salvando o contexto atual */
-	swapcontext(&(atual->context), &(proxima->context));
-	/* Retorno com sucesso */
-	return 0;
+    filatask_t *proxima = filaTarefas, *atual = filaTarefas;
+    if (atual == NULL)
+    {
+        fprintf(stderr, "Fila vazia?\n");
+        return -3;
+    }
+    for (;;)
+    {
+        proxima = proxima->next;
+        if (proxima == NULL)
+        {
+            fprintf(stderr, "Elemento nulo detectado - task_switch\n");
+            return -1;
+        }
+        if (proxima == filaTarefas)
+        {
+            fprintf(stderr, "Tarefa nao encontrada - task_switch\n");
+            return -2;
+        }
+        if (proxima == task)
+            break;
+    }
+    /* Setamos a tarefa a ser trocada como a tarefa ativa */
+    filaTarefas = proxima;
+    /* E trocamos de contexto, salvando o contexto atual */
+    swapcontext(&(atual->task->context), &(proxima->task->context));
+    /* Retorno com sucesso */
+    return 0;
 }
 
 // Termina a tarefa e volta para a tarefa 'main'.
 //      - Deve ser usado a função swap_task para voltar para o main
 void task_exit(int exit_code)
 {
-	filatask_t *atual = filaTarefas, *proxima = filaTarefas;
-    	for(;;)
-    	{
-		proxima = proxima->next;
-		if(proxima == NULL){
-	       		fprintf(stderr, "Elemento nulo detectado - task_switch\n");
-			return -1;
-		}
-		if(proxima == filaTarefas){
-			fprintf(stderr, "Main nao encontrada - task_switch\n");
-			return -2;
-		}
-		/* Se encontrou a main, break */
-		if(proxima->id == 0) break;
-    	}
-	/* Removemos a tarefa e desalocamos */
-	queue_remove(&filaTarefas, atual);
-	free(atual);
+    filatask_t *atual = filaTarefas, *proxima = filaTarefas;
+    if (filaTarefas == NULL)
+    {
+        fprintf(stderr, "Fila vazia?\n");
+        return -3;
+    }
+    for (;;)
+    {
+        proxima = proxima->next;
+        if (proxima == NULL)
+        {
+            fprintf(stderr, "Elemento nulo detectado - task_switch\n");
+            return -1;
+        }
+        if (proxima == filaTarefas)
+        {
+            fprintf(stderr, "Main nao encontrada - task_switch\n");
+            return -2;
+        }
+        /* Se encontrou a main, break */
+        if (proxima->task->id == 0)
+            break;
+    }
+    /* Removemos a tarefa e desalocamos */
+    queue_remove(&filaTarefas, atual);
+    free(atual);
 
-	/* Trocamos para a main */
-	if(swap_task(proxima) < 0) fprintf(stderr, "Erro ao trocar para a main - task_exit\n");
+    /* Trocamos para a main */
+    if (swap_task(proxima) < 0)
+        fprintf(stderr, "Erro ao trocar para a main - task_exit\n");
 }
 
 // Retorna o identificador da tarefa atual.
@@ -122,5 +138,5 @@ void task_exit(int exit_code)
 //      - Não podem existir duas tarefas com o mesmo nome
 int task_id()
 {
-	return filaTarefas->id;
+    return filaTarefas->task->id;
 }
