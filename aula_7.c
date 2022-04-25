52f503fde9c6876ba88a45c8aa7e50fbc47dab55
Paulo Sergio Avila Junior <pieraquara@gmail.com>
2022-04-25 17:00:48 -0300

    esqueleto aula 7

---
+++ aula_7.c
@@ -0,0 +1,26 @@
+/* Paulo Sergio Avila Junior e Mariana Gomes Luz */
+/* Aula 7 - espera ocupada */
+
+#include <stdlib.h>
+#include <queue.h>
+/* Funcao das threads */
+void threadFxn()
+{
+	/* Retira primeiro elemento da fila e salva em uma variavel */
+
+	/* Cria valor novo aleatorio */
+
+	/* Poe novo valor na fila */
+
+	/* Imprime a operacao realizada */
+}
+
+void main()
+{
+	/* Cria thread 1 */
+
+	/* Cria thread 2 */
+
+
+	exit(0);
+}
